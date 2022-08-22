a15bdf19564f2e89595ce623b649ce18ee7078fe
grahamwwilson <gwwilson@ku.edu>
2022-08-22 13:53:23 -0500

    Collate.sh

--- Collate.sh
+++ Collate.sh
@@ -5,8 +5,8 @@
 # Need to source ROOT for hadd step
 . ./setenv.sh
 
-mkdir Collating/Eta-0p1
-cd Collating/Eta-0p1
+mkdir Collating/Eta-1p25
+cd Collating/Eta-1p25
 
 execdir=../../pc_ExecutionDirectory
 jobfile=../../Lists/jobs.list
@@ -19,7 +19,7 @@ for job in $(cat ${jobfile})
 do
 # Make symbolic link in collation subdirectory
    echo 'job: '$job
-   mydir=MinBias2018_Rsim_GWW_ALL-J-${job}-0-12-V2
+   mydir=MinBias2018_Rsim_GWW_ALL-J-${job}-0-12-V3
    ofile=" Outfile${job}.root"
    ln -s ${execdir}/${mydir}/Outfile.root ${ofile}
    command+="${ofile}"
