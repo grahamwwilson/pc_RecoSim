b76767696cfd74c91d47954dcd973136ae165716
grahamwwilson <gwwilson@ku.edu>
2022-08-22 13:37:13 -0500

    Add array and task id

--- job_array.sh
+++ job_array.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=conv2                # Job name
+#SBATCH --job-name=pc                   # Job name
 #SBATCH --partition=sixhour             # Partition Name (Required)
 #SBATCH --mail-type=ALL                 # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=gwwilson@ku.edu     # Where to send mail	
@@ -7,7 +7,8 @@
 #SBATCH --cpus-per-task=12              # Number of threads to use
 #SBATCH --mem=2gb                       # Job memory request
 #SBATCH --time=0-06:00:00               # Time limit days-hrs:min:sec
-#SBATCH --output=conv2_%j.log           # Standard output and error log
+#SBATCH --output=pc_%A_%a.log           # Standard output
+#SBATCH --error=pc_%A_%a.err           # Standard error
 #SBATCH --array=1-12                    # Job array
 
 echo 'SLURM_ARRY_TASK_ID: '$SLURM_ARRAY_TASK_ID
@@ -15,7 +16,7 @@ echo 'SLURM_ARRY_TASK_ID: '$SLURM_ARRAY_TASK_ID
 LIST="MinBias2018_Rsim_GWW_ALL-J-"$SLURM_ARRAY_TASK_ID
 NUMFILES=0
 NTHREADS=12
-VERSION=2
+VERSION=3
 
 pwd
 hostname
