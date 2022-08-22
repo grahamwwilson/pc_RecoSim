65377b349b2602a4be5400c8150f6f06d321894f
grahamwwilson <gwwilson@ku.edu>
2022-08-22 08:51:29 -0500

    mods

--- Collate.sh
+++ Collate.sh
@@ -2,7 +2,7 @@
 #
 # Collate jobs and add output histograms
 #
-#Need to source ROOT
+# Need to source ROOT
 . ./setenv.sh
 
 mkdir Collating/Eta-0p7
@@ -13,12 +13,18 @@ jobfile=../../Lists/jobs.list
 masterlist="MinBias2018_Rsim_GWW_ALL"
 masterlistfile=${masterlist}.list
 
+command="hadd Outfile.root"
+
 for job in $(cat ${jobfile})
 do
 # Make symbolic link in collation subdirectory
    echo 'job: '$job
    mydir=MinBias2018_Rsim_GWW_ALL-J-${job}-0-12-V1
-   ln -s ${execdir}/${mydir}/Outfile.root Outfile${job}.root
+   ofile=" Outfile${job}.root"
+   ln -s ${execdir}/${mydir}/Outfile.root ${ofile}
+   command+="${ofile}
 done
+
+echo ${command}
 
 exit
