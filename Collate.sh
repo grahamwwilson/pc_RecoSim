c274074fd85b3f9020b3fbaa465bc3ecaf180902
grahamwwilson <gwwilson@ku.edu>
2022-11-24 11:21:36 -0600

    update jobs

--- Collate.sh
+++ Collate.sh
@@ -19,7 +19,7 @@ for job in $(cat ${jobfile})
 do
 # Make symbolic link in collation subdirectory
    echo 'job: '$job
-   mydir=MinBias2018_Rsim_GWW_ALL-J-${job}-0-12-V4
+   mydir=MinBias2018_Rsim_GWW_ALL-J-${job}-0-12-V5
    ofile=" Outfile${job}.root"
    ln -s ${execdir}/${mydir}/Outfile.root ${ofile}
    command+="${ofile}"
